be40c80402c0e8ac0ae190a699c169e71f0b385f
DenSad81 <tagil81@mail.ru>
2024-04-14 10:32:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        string firstName = "Sadakov";
+        string lastName = "Denis";
+        string cup1 = "Tea";
+        string cup2 = "Coffe";
+        string tempValue;
+
+        Console.WriteLine($"Имя: {firstName} Фамилия: {lastName}");
+        tempValue = firstName;
+        firstName = lastName;
+        lastName = tempValue;
+        Console.WriteLine($"Имя: {firstName} Фамилия: {lastName}");
+        Console.Write('\n');
+
+        Console.WriteLine($"Чашка 1: {cup1} Чашка 2: {cup2}");
+        tempValue = cup1;
+        cup1 = cup2;
+        cup2 = tempValue;
+        Console.WriteLine($"Чашка 1: {cup1} Чашка 2: {cup2}");
+    }
+}
+
